48f4b5d67ee77ded71250b40702562be7e022f4d
Dilip <32507121+Dilipjp@users.noreply.github.com>
2024-11-06 00:28:03 -0500

    Pages UI updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,13 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation ("com.google.android.libraries.places:places:2.6.0") // Add this line
+
 
 
 
